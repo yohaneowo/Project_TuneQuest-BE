caa372826aaa053e4c29d234eaf66900d716b14e
Yohanewww <yohane910@gmail.com>
2024-01-11 21:15:44 +0800

    Remove temporary files and directories from version control
    This commit removes the .temp_outputFolder/ and .idea/ directories from version control. These directories contain temporary files and IDE-specific settings that should not be tracked in the repository.

--- ai_generation_core/musicgen/musicgen.py
+++ ai_generation_core/musicgen/musicgen.py
@@ -5,7 +5,7 @@ import random
 import replicate
 
 class Musicgen:
-    def __init__(self, top_k: int, top_p: int, prompt: str, duration: int, temperature: int, continuation: bool, model_version: str, output_format: str, continuation_start: int, multi_band_diffusion: bool, normalization_strategy: str, classifier_free_guidance: int,  generate_method: str):
+    def __init__(self, top_k: int, top_p: int, prompt: str, duration: int, temperature: int, continuation: bool, model_version: str, output_format: str, continuation_start: int, multi_band_diffusion: bool, normalization_strategy: str, classifier_free_guidance: int,  generate_method: str , audiofile_path: str, mission_id: str):
         self.top_k = top_k
         self.top_p = top_p
         self.prompt = prompt
@@ -19,6 +19,8 @@ class Musicgen:
         self.normalization_strategy = normalization_strategy
         self.classifier_free_guidance = classifier_free_guidance
         self.generate_method = generate_method
+        self.audiofile_path = audiofile_path
+        self.mission_id = mission_id
 
     def generate_music(self):
         if self.generate_method == 'replicate':
@@ -48,7 +50,8 @@ class Musicgen:
         print(output)
 
     def generate_music_by_local(self):
-        model = MusicGen.get_pretrained('facebook/musicgen-stereo-melody-large')
+        audiofile_path = self.audiofile_path
+        model = MusicGen.get_pretrained('facebook/musicgen-melody')
         model.set_generation_params(
             top_k=self.top_k,
             top_p=self.top_p,
@@ -57,14 +60,12 @@ class Musicgen:
             cfg_coef=self.classifier_free_guidance,
         )  # generate 8 seconds.
 
-        descriptions = ['happy rock in modern style with electric guitar and drums']
-
-        melody, sr = torchaudio.load('./bach.mp3')
+        descriptions = self.prompt
+        melody, sr = torchaudio.load(self.audiofile_path)
         # generates using the melody from the given audio and the provided descriptions.
         wav = model.generate_with_chroma(descriptions, melody[None].expand(1, -1, -1), sr, True)
 
-        for idx, one_wav in enumerate(wav):
-            # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
-            audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness")
+        # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+        audio_write(f'{self.mission_id}', wav.cpu(), model.sample_rate, strategy="loudness")
 
 

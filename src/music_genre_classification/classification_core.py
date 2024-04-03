06e79d8f824cb9c88905d8444cd36c3231f1b26e
Yohanewww <yohane910@gmail.com>
2024-04-03 10:42:11 +0800

    typo 修正

--- src/music_genre_classification/classification_core.py
+++ src/music_genre_classification/classification_core.py
@@ -17,11 +17,11 @@ scaler_filepath = "./src/music_genre_classification/scaler.save"
 sc = joblib.load(scaler_filepath)
 genre_map = {0: 'Blues',
              1: 'Classical',
-             2: 'County',
+             2: 'Country',
              3: 'Disco',
              4: 'Hip-hop',
              5: 'jazz',
-             6: 'Metel',
+             6: 'Metal',
              7: 'Pop',
              8: 'Reggae',
              9: 'Rock'}
